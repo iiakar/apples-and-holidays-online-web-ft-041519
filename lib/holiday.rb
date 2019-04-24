23e85f69bf8851f7cf159d01350e65fda019e663
Ibrahim  Akar <ibsakar@gmail.com>
2019-04-24 12:14:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,30 +1,37 @@
 require 'pry'
 
-def second_supply_for_fourth_of_july(holiday_hash)
-  # given that holiday_hash looks like this:
-  # {
-  #   :winter => {
-  #     :christmas => ["Lights", "Wreath"],
-  #     :new_years => ["Party Hats"]
-  #   },
-  #   :summer => {
-  #     :fourth_of_july => ["Fireworks", "BBQ"]
-  #   },
-  #   :fall => {
-  #     :thanksgiving => ["Turkey"]
-  #   },
-  #   :spring => {
-  #     :memorial_day => ["BBQ"]
-  #   }
-  # }
-  # return the second element in the 4th of July array
+def second_supply_for_fourth_of_july
+        expect(second_supply_for_fourth_of_july(holiday_supplies)).to eq("BBQ")
+.to_s
+  holiday_hash = 
+   {
+     :winter => {
+       :christmas => ["Lights", "Wreath"],
+       :new_years => ["Party Hats"]
+     },
+     :summer => {
+       :fourth_of_july => ["Fireworks", "BBQ"]
+     },
+     :fall => {
+       :thanksgiving => ["Turkey"]
+     },
+     :spring => {
+       :memorial_day => ["BBQ"]
+     },
+     :BBQ => {}
+       
+     }
+   }
+   return the second element in the 4th of July array
 end
 
-def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
+      expect(second_supply_for_fourth_of_july(holiday_supplies)).to eq("BBQ")
 
+def add_supply_to_winter_holidays(holiday_hash, supply) 
+     {
+     :winter => {
+       :christmas => ["Lights", "Wreath", "Balloons"],
+  
 end
 
 
